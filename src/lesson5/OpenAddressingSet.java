ecc56e81afaa34c46d5bf6ca7bb2a4ec486b7dce
linome75 <hercvildendi@gmail.com>
2022-05-07 22:07:46 +0300

    4 lesson

--- src/lesson5/OpenAddressingSet.java
+++ src/lesson5/OpenAddressingSet.java
@@ -1,10 +1,11 @@
 package lesson5;
 
-import kotlin.NotImplementedError;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.AbstractSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class OpenAddressingSet<T> extends AbstractSet<T> {
@@ -16,6 +17,7 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
     private final Object[] storage;
 
     private int size = 0;
+    private final Object deleted = new Object();
 
     private int startingIndex(Object element) {
         return element.hashCode() & (0x7FFFFFFF >> (31 - bits));
@@ -43,7 +45,7 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
         int index = startingIndex(o);
         Object current = storage[index];
         while (current != null) {
-            if (current.equals(o)) {
+            if (current.equals(o) && storage[index] != deleted) {
                 return true;
             }
             index = (index + 1) % capacity;
@@ -54,10 +56,10 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
 
     /**
      * Добавление элемента в таблицу.
-     *
+     * <p>
      * Не делает ничего и возвращает false, если такой же элемент уже есть в таблице.
      * В противном случае вставляет элемент в таблицу и возвращает true.
-     *
+     * <p>
      * Бросает исключение (IllegalStateException) в случае переполнения таблицы.
      * Обычно Set не предполагает ограничения на размер и подобных контрактов,
      * но в данном случае это было введено для упрощения кода.
@@ -67,7 +69,7 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
         int startingIndex = startingIndex(t);
         int index = startingIndex;
         Object current = storage[index];
-        while (current != null) {
+        while (current != null && storage[index] != deleted) {
             if (current.equals(t)) {
                 return false;
             }
@@ -84,34 +86,79 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
 
     /**
      * Удаление элемента из таблицы
-     *
+     * <p>
      * Если элемент есть в таблица, функция удаляет его из дерева и возвращает true.
      * В ином случае функция оставляет множество нетронутым и возвращает false.
      * Высота дерева не должна увеличиться в результате удаления.
-     *
+     * <p>
      * Спецификация: {@link Set#remove(Object)} (Ctrl+Click по remove)
-     *
+     * <p>
      * Средняя
+     * <p>
+     * Трудоемкость O(n)
+     * Ресурсоемкость O(1)
      */
     @Override
     public boolean remove(Object o) {
-        return super.remove(o);
+        if (contains(o)) {
+            int targetIndex = startingIndex(o);
+            if (storage[targetIndex].equals(o)) {
+                storage[targetIndex] = deleted;
+                size--;
+                return true;
+            }
+            int nextIndex = (targetIndex + 1) % capacity;
+            while (nextIndex != targetIndex) {
+                if (storage[nextIndex].equals(o)) {
+                    storage[nextIndex] = deleted;
+                    size--;
+                    return true;
+                }
+                nextIndex = (nextIndex + 1) % capacity;
+            }
+        }
+        return false;
     }
 
     /**
      * Создание итератора для обхода таблицы
-     *
+     * <p>
      * Не забываем, что итератор должен поддерживать функции next(), hasNext(),
      * и опционально функцию remove()
-     *
+     * <p>
      * Спецификация: {@link Iterator} (Ctrl+Click по Iterator)
-     *
+     * <p>
      * Средняя (сложная, если поддержан и remove тоже)
+     * <p>
+     * Трудоемкость
+     * Ресурсоемкость
      */
     @NotNull
     @Override
     public Iterator<T> iterator() {
-        // TODO
-        throw new NotImplementedError();
+        return new OpenAddressingSetIterator();
+    }
+
+    public class OpenAddressingSetIterator implements Iterator<T> {
+        int index = 0;
+        int objIndex = 0;
+        T object;
+
+        @Override
+        public boolean hasNext() {
+            return objIndex < size;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) {
+                while (storage[index] == null || storage[index].equals(deleted)) index++;
+                object = (T) storage[index];
+                index++;
+                objIndex++;
+                return object;
+            }
+            throw new NoSuchElementException();
+        }
     }
 }
